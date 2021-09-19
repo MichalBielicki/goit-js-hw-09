b93b737426c78f661ff65887809dcf3fa1124f11
MichalBielicki <80898561+MichalBielicki@users.noreply.github.com>
2021-09-19 12:57:19 +0200

    zad 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let backgroundColor;
+const colors = () => {
+  body.style.backgroundColor = getRandomHexColor();
+};
+const workColors = () => {
+  stopBtn.disabled = false;
+  backgroundColor = setInterval(colors, 1000);
+  startBtn.disabled = true;
+};
+const notWorkColors = () => {
+  stopBtn.disabled = true;
+  clearInterval(backgroundColor);
+  startBtn.disabled = false;
+};
+startBtn.addEventListener('click', workColors);
+stopBtn.addEventListener('click', notWorkColors);
---
+++ src/js/02-timer.js
@@ -0,0 +1,58 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const qs = selector => document.querySelector(selector);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const calendar = qs('#date-selector');
+const btnStart = qs('button[data-start]');
+const daysOutput = document.querySelector('.timer span[data-days]');
+const hoursOutput = document.querySelector('.field span[data-hours]');
+const minutesOutput = document.querySelector('.field span[data-minutes]');
+const secondsOutput = document.querySelector('.field span[data-seconds');
+
+let currentTime = new Date().getTime();
+let choosenTime = 0;
+let remainingTime = 0;
+let currentValue = '';
+
+btnStart.setAttribute('disabled', true);
+
+const timeCheck = () => {
+  if (currentTime > choosenTime) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+  } else {
+    btnStart.removeAttribute('disabled');
+  }
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+const fp = flatpickr(calendar, opts);
+btnStart.addEventListener('click', countdown);
